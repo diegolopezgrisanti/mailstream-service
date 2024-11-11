5a0fc01e3c09c46d577bc131574496269c8485eb
diegolopezgrisanti <diegolopezgrisanti@gmail.com>
2024-11-11 10:53:26 -0800

    add Spring Boot Dependency

---
+++ src/main/java/com/mailstream/MailStreamServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mailstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailStreamServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailStreamServiceApplication.class, args);
+    }
+}
