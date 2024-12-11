79ef80eb4b80c0ef7e5a6191e5164697c7e085e1
Diego López Grisanti <diegolopezgrisanti@gmail.com>
2024-12-11 15:23:28 -0800

    Add Lombok Dependency (#3)

--- src/main/java/com/mailstream/domain/subscriber/Subscriber.java
+++ src/main/java/com/mailstream/domain/subscriber/Subscriber.java
@@ -1,40 +1,17 @@
 package com.mailstream.domain.subscriber;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class Subscriber {
     private UUID id;
     private String email;
     private LocalDateTime subscriptionDate;
-
-    public Subscriber(UUID id, String email, LocalDateTime subscriptionDate) {
-        this.id = id;
-        this.email = email;
-        this.subscriptionDate = subscriptionDate;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDateTime getSubscriptionDate() {
-        return subscriptionDate;
-    }
-
-    public void setSubscriptionDate(LocalDateTime subscriptionDate) {
-        this.subscriptionDate = subscriptionDate;
-    }
 }
